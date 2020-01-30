d6fcb25008a9043944f765c8802a24e0bfee4b18
Lehan-Max <lehanmx14@gmail.com>
2020-01-30 22:42:32 +0530

    1 file modified

--- src/app/ipl.service.ts
+++ src/app/ipl.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/http'
 
-@Injectable({
-  provideIn 'root'
-  })
+@Injectable()
 export class IplService {
 
   constructor() { }
